5e58d38a4e9d6bba4d6b0708d648a7babcd9f251
JoshuaLQuartey <joshualkquartey@gmail.com>
2024-04-01 15:24:51 +0100

    Using requests to get page with players in squad

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print('Hello Heaven')
+import requests 
+
+players_url =  "https://proclubshead.com/24/club-squad/gen5-179156/" #url of the page with all squad members link
+data = requests.get(players_url) 
