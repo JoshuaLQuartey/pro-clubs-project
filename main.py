470dc052194a1d58b23f148afa0046ca2910dbfa
JoshuaLQuartey <joshualkquartey@gmail.com>
2024-04-01 15:34:04 +0100

    removed comment

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import requests 
 
-players_url =  "https://proclubshead.com/24/club-squad/gen5-179156/" #url of the page with all squad members link
+players_url =  "https://proclubshead.com/24/club-squad/gen5-179156/" 
 data = requests.get(players_url) 
