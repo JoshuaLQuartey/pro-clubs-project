58c3ab01eac2fb056503ce2cc10a1d89499fd021
JoshuaLQuartey <joshualkquartey@gmail.com>
2024-04-01 16:02:48 +0100

    Getting the stats for the first player on the page

--- main.py
+++ main.py
@@ -10,4 +10,9 @@ links = members.find_all('a')
 links = (l.get("href") for l in links) 
 links = [l for l in links if '/club-player/']
 player_url = [f"https://proclubshead.com/{l}" for l in links]  
-print(player_url)
+
+data = requests.get(player_url[0])
+soup = BeautifulSoup(data.text, 'html.parser')
+current_table = soup.select('div.flex-nowrap')[1] #selected the table on the html i want #Extract data from the table
+table_soup = BeautifulSoup(str(current_table), 'html.parser')
+print(table_soup)
