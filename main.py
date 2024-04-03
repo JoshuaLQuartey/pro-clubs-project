a89c34ed022b147e47a0893c6be704a6a5789e56
JoshuaLQuartey <joshualkquartey@gmail.com>
2024-04-03 12:06:21 +0100

    changing varibles

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ players_url =  "https://proclubshead.com/24/club-squad/gen5-179156/"
 data = requests.get(players_url) 
 
 soup = BeautifulSoup(data.text) 
-members = soup.select('div.bg-gradient')[0] 
-links = members.find_all('a') 
-links = (l.get("href") for l in links) 
-links = [l for l in links if '/club-player/']
-player_url = [f"https://proclubshead.com/{l}" for l in links]  
 
-data = requests.get(player_url[0])
+squad_list_div = soup.select('div.bg-gradient')[0] 
+player_links = squad_list_div.find_all('a') 
+player_hrefs = (link.get("href") for link in player_links) 
+player_slugs = [links for links in player_hrefs if '/club-player/']
+player_urls = [f"https://proclubshead.com/{slug}" for slug in player_slugs]  
+
+data = requests.get(player_urls[0])
 soup = BeautifulSoup(data.text, 'html.parser')
 current_table = soup.select('div.flex-nowrap')[1] 
 table_soup = BeautifulSoup(str(current_table), 'html.parser')
