046a26db3f9ff1e8798c52068dd418cd74794a56
JoshuaLQuartey <joshualkquartey@gmail.com>
2024-05-21 11:46:20 +0100

    Taking data and putting into supabase dataframe

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests 
 from bs4 import BeautifulSoup
 import pandas as pd
+import json
 
 squad_url =  "https://proclubshead.com/24/club-squad/gen5-179156/" 
 data = requests.get(squad_url) 
@@ -17,7 +18,7 @@ all_data = []
 for player_url in player_urls:
     data = requests.get(player_url)
     soup = BeautifulSoup(data.text, 'html.parser')
-    current_table = soup.select('div.flex-nowrap')[1]  
+    current_table = soup.select('div.flex-nowrap')  
     table_soup = BeautifulSoup(str(current_table), 'html.parser')
 
     player_name = player_url.split("/")[-2].replace("gen5-179156-", "") 
@@ -37,3 +38,28 @@ df = pd.DataFrame(all_data)
 df_exploded = df.apply(pd.Series.explode) 
 df_exploded.reset_index(drop=True, inplace=True)
 print(df_exploded)
+
+records = df_exploded.to_dict(orient='records')
+
+supabase_url = 'https://wqdoemgzcvnhviizniim.supabase.co'
+supabase_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxZG9lbWd6Y3ZuaHZpaXpuaWltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE1NDM5MzAsImV4cCI6MjAyNzExOTkzMH0.-40hkzpWnjWp3baLZ28XoonI6Yvk4vM3QGTe_HOwjYs'
+table_name = 'playerstats'
+
+for record in records:
+    record['stat_name'] = record['stat_name'].text
+    record['stat_value'] = record['stat_value'].text
+    
+headers = {
+    'Content-Type': 'application/json',
+    'apikey': supabase_key,
+}
+
+insert_url = f"{supabase_url}/rest/v1/{table_name}"
+response = requests.post(insert_url, headers=headers, data=json.dumps(records))
+
+print(response.text)
+
+if response.status_code == 201:
+    print("Records inserted successfully into Supabase table!")
+else:
+    print(f"Failed to insert records. Status code: {response.status_code}, Response: {response.text}")
